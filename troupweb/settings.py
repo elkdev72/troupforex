cc86272c330520a7dbff3a93757e9251ae94db32
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-28 22:33:37 +0300

    settings.py

--- troupweb/settings.py
+++ troupweb/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "forex",
+    'users',
+    'crispy_forms',
+    'crispy_bootstrap4',
     
 ]
 
@@ -123,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
