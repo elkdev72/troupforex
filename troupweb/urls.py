14dbadf1fc74e05c0e36532ac2907db6a05c6855
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-28 22:32:14 +0300

    users urls

--- troupweb/urls.py
+++ troupweb/urls.py
@@ -20,6 +20,8 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("forex.urls")),
+    path('accounts/', include('users.urls')),  # Changed from 'users/' to 'accounts/'
+
     
 
 ]
